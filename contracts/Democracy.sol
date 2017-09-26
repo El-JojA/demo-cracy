ec206b40bef6d7b9932d47b1f9f3bf4c3d0d1055
Mr. Joja <jjcardonap@gmail.com>
2017-09-26 03:57:06 -0600

    erased getters after realizing there's no need to have them, refactoring Law.sol and Democracy.sol still WIP i fucked up with my mapping logic, needs refactor.

--- contracts/Democracy.sol
+++ contracts/Democracy.sol
@@ -3,8 +3,48 @@
     Where everybody (address) have the same right to vote and every vote
     have the same weight.
 */
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.15;
 
-contract Democracy{
-    
+
+contract Democracy {
+
+    // mapping(Law.address, [voter, vote(+1/0/-1)])
+    struct vote {
+        int8 voteType;
+        bool isValid;
+    }
+
+    mapping (address=>mapping (address=>vote)) votes;
+    uint256 democracyStartDate;
+    string democracyName;
+
+    enum LawStatus { Invote, Passed, Denied }
+    enum VoteType { Null, No, Yes  }
+
+    modifier castedVote(address _law) {
+        require(votes[_law][msg.sender].isValid == false);
+        _;
+    }
+
+    function Democracy(string _democracyName) public {
+        democracyName = _democracyName;
+        democracyStartDate = now;
+    }
+
+    //I saw a need for 3 different functions to avoid exploits on sending a not permited parameter.
+    //Probably there is a clever way of doing this, if you know it please fix this. It's 3AM and I should be sleeping to wake up at least at 7AM. 
+    function voteYes(address _law) public castedVote(_law) {
+        votes[_law][msg.sender].voteType = 1;
+    }
+
+
+    function voteNo(address _law) public castedVote(_law) {
+        votes[_law][msg.sender].voteType = -1;
+    }
+
+
+    function voteNull(address _law) public castedVote(_law) {
+        votes[_law][msg.sender].voteType = 0;
+    }
+
 }
